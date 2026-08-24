53848307c9fc13961161f99c078d5a58180d601b
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-08-24 09:05:36 +0600

    Add TooniboyPlugin.kt entry point

---
+++ TooniboyProvider/src/main/kotlin/com/tooniboy/TooniboyPlugin.kt
@@ -0,0 +1,20 @@
+package com.tooniboy
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class TooniboyPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Tooniboy())
+        registerExtractorAPI(Zephyrflick())
+        registerExtractorAPI(Abyss())
+        registerExtractorAPI(StreamRuby())
+        registerExtractorAPI(Cloudy())
+        registerExtractorAPI(GDMirrorbot())
+        registerExtractorAPI(GDMirrorbotFHD())
+        registerExtractorAPI(EmTurboVid())
+        registerExtractorAPI(VidMolyNet())
+        registerExtractorAPI(Blakite())
+    }
+}
